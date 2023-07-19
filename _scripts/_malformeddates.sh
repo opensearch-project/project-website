8aabbb684fd6a8073c6b9105799f1c69af63056b
David Tippett <dtip@amazon.com>
2023-07-19 14:19:40 -0400

    Fixed alformed events date
    Signed-off-by: David Tippett <dtip@amazon.com>

--- _scripts/_malformeddates.sh
+++ _scripts/_malformeddates.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-malformed_event_dates=$(grep -Er --files-without-match '^eventdate: [0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} [-+][0-9]{4}$' _events)\
+malformed_event_dates=$(grep -Er --files-without-match '^eventdate: [0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} [-+][0-9]{4}$' _events)
 if [ -n "${malformed_event_dates}" ]; then
 echo "Malformed event date in:" >&2
 echo "${malformed_event_dates}" >&2
