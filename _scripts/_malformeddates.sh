3569e039da3fe9663691ba54fdef174d73198e54
David Tippett <Dtaivpp@gmail.com>
2023-08-02 11:21:18 -0400

    Update _scripts/_malformeddates.sh
    Co-authored-by: Romain Tartière <romain@blogreen.org>
    Signed-off-by: David Tippett <Dtaivpp@gmail.com>

--- _scripts/_malformeddates.sh
+++ _scripts/_malformeddates.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 malformed_event_dates=$(grep -Er --files-without-match '^eventdate: [0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} [-+][0-9]{4}$' _events)
 if [ -n "${malformed_event_dates}" ]; then
