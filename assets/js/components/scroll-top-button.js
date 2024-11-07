d6ee70da22371cf7e78b5eec869e90bf2f7163ce
kaimmej <kaimmej@gmail.com>
2024-11-07 16:59:25 -0500

    Scroll to top button
    created the scroll to top component, the javascript and css files.
    
    Signed-off-by: kaimmej <kaimmej@gmail.com>

---
+++ assets/js/components/scroll-top-button.js
@@ -0,0 +1,59 @@
+/**
+ * Animate scroll to top button in/off view
+ */
+
+export default (() => {
+    let button = document.querySelector('.btn-scroll-top'),
+      scrollOffset = 500
+  
+    if (button === null) return
+  
+    let offsetFromTop = parseInt(scrollOffset, 10),
+      progress = button.querySelector('svg rect'),
+      length = progress.getTotalLength()
+  
+    progress.style.strokeDasharray = length
+    progress.style.strokeDashoffset = length
+  
+    const showProgress = () => {
+      let scrollPosition = window.pageYOffset,
+        scrollHeight =
+          document.documentElement.scrollHeight -
+          document.documentElement.clientHeight,
+        scrollPercent = scrollPosition / scrollHeight || 0,
+        draw = length * scrollPercent
+      progress.style.strokeDashoffset = length - draw
+    }
+  
+    const handleScroll = () => {
+      const toggleButtonVisibility = () => {
+        if (window.pageYOffset > offsetFromTop) {
+          button.classList.add('show')
+        } else {
+          button.classList.remove('show')
+        }
+      }
+  
+      const throttledToggleButtonVisibility = throttle(
+        toggleButtonVisibility,
+        100
+      )
+  
+      throttledToggleButtonVisibility()
+      showProgress()
+    }
+  
+    const throttle = (callback, delay) => {
+      let timeoutId = null
+      return function () {
+        if (timeoutId === null) {
+          timeoutId = setTimeout(() => {
+            callback.apply(null, arguments)
+            timeoutId = null
+          }, delay)
+        }
+      }
+    }
+  
+    window.addEventListener('scroll', handleScroll)
+  })()
